f517a38e875d50c1c5712dd0092852cc59cf5b29
omjadhav <omjadhav@omjadhavs-MacBook-Air.local>
2024-01-27 10:23:36 +0530

    commit

---
+++ TicTacToe.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printBoard(const vector<vector<char>>& board) {
+    for (const auto& row : board) {
+        for (char cell : row) {
+            cout << cell << " | ";
+        }
+        cout << endl << "---------" << endl;
+    }
+}
+
+bool checkWinner(const vector<vector<char>>& board, char player) {
+    // Check rows and columns
+    for (int i = 0; i < 3; ++i) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+
+    return false;
+}
+
+bool isBoardFull(const vector<vector<char>>& board) {
+    for (const auto& row : board) {
+        for (char cell : row) {
+            if (cell == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    vector<vector<char>> board(3, vector<char>(3, ' '));
+    char currentPlayer = 'X';
+
+    while (true) {
+        printBoard(board);
+
+        int row, col;
+        cout << "Player " << currentPlayer << ", enter row (0, 1, 2): ";
+        cin >> row;
+        cout << "Player " << currentPlayer << ", enter column (0, 1, 2): ";
+        cin >> col;
+
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = currentPlayer;
+
+            if (checkWinner(board, currentPlayer)) {
+                printBoard(board);
+                cout << "Player " << currentPlayer << " wins!" << endl;
+                break;
+            } else if (isBoardFull(board)) {
+                printBoard(board);
+                cout << "It's a tie!" << endl;
+                break;
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        } else {
+            cout << "Invalid move. Try again." << endl;
+        }
+    }
+
+    return 0;
+}
